fd1478ee5ebb59cd7cfc00e15ea7ca0d7e7f98dc
donniekramer8 <donniekramer@gmail.com>
2022-11-28 21:42:23 -0500

    removed unused files

--- ncbi/testncbi.py
+++
@@ -1,5 +0,0 @@
-from Bio import Entrez
-
-Entrez.email = "donniekramer@gmail.com"
-handle = Entrez.efetch(db="nucleotide", id="AY851612", rettype="gb", retmode="text")
-print(handle.readline().strip())
